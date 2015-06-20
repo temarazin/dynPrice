044edb3a5043ae4644012bed2b12188809ff37bb
Артем <temarazin@gmail.com>
2015-06-20 04:07:50 +0400

    0.1.1

--- dynPrice.js
+++ dynPrice.js
@@ -2,37 +2,47 @@
  * Created by temarazin on 18.06.2015.
  */
 
-/*
-
- css class .dp-quality
- css class .dp-cost
- css class .dp-result
- css class .dp-total
-
-*/
 $(document).ready(function() {
 
-    $('.dp-quality').click(function () {
-        if ($('input').is('.inp_kol') == false) {
-            k = $(this).text();
-            $(this).html('<input class="inp_kol" type="number" min=0 value="' + k + '">');
-            $('.inp_kol').focus();
-        }
-    });
+    var quality = $('.dp-quality');	// Êîëè÷åñòâî
+    var cost = $('.dp-cost');		// Öåíà
+	var res = $('.dp-result');		// Ñóììà
+	var total = $('.dp-total');		// ÈÒÎÃÎ
 
-    $('.dp-quality').focusout(function () {
-        k = $('.inp_kol').val();
-        $(this).text(k);
-        //$(this).siblings('.p_sum').text(parseFloat($(this).siblings('.p_cost').text()) * k);
-        $(this).siblings('.dp-result').text(parseFloat($(this).siblings('.dp-cost').text().replace(/[^0-9]*/,"")) * k);
-        s = 0;
-        $('.dp-result').each(function() {
-            t = $(this).text();
-            t = t.replace(/\D\./g,'');
-            s += Number(t);
-        });
 
-        (s > 0) ? ($('.dp-total').text('Total: ' + s + '$')) : ($('.dp-total').text(''));
-    });
+    var k;
+
+	function changePrice(coun) {
+		var slct = $(coun).parent('.dp-quality');
+		var t;
+		k = $(coun).val();
+		slct.siblings('.dp-result').text(parseFloat(slct.siblings('.dp-cost').text().replace(/[^0-9]*/,"")) * k);
+		var s = 0;
+		res.each(function() {
+			t = $(this).text();
+			t = t.replace(/\D\./g,'');
+			s += Number(t);
+		});
+		(s > 0) ? (total.text('Total: ' + s + '$')) : (total.text(''));
+	}
+
+	    quality.click(function () {
+	        if ($('input').is('.inp_kol') == false) {
+	            k = $(this).text();
+	            $(this).html('<input class="inp_kol" type="number" min=0 value="' + k + '">');
+
+	            $('.inp_kol').focus();
+
+				$('.inp_kol').bind("change", function () {
+					changePrice(this);
+
+				});
+
+				$('.inp_kol').bind("focusout", function () {
+					changePrice(this);
+					$(this).parent().text(k);
+				});
+	        }
+	    });
 
 });
